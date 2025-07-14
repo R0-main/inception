7284e203c1a90484395a58108484c4a271a34fc3
R0-main <justromixx@gmail.com>
2025-07-14 14:47:08 +0200

    fqwfqf

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# wp core install
+# wp user create --allow-root rguigneb rguigneb@example.com --user_pass=admin --role=administrator
+wp user create --allow-root riad riad@example.com --user_pass=riadlebg --role=subscriber
+wp core install --allow-root --url=rguigneb.42.fr --title=42 --admin_user=rguigneb --admin_password=admin --admin_email=rguigneb@gmail.com
+
 # Start PHP-FPM
 echo "Starting PHP-FPM..."
 exec /usr/sbin/php-fpm8.2 -F
