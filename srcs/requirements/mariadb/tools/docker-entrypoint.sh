2820fea31691f683e05a57dbfaaba86149c48183
R0-main <justromixx@gmail.com>
2025-07-15 10:33:46 +0200

    remove unused comments

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Use envsubst to substitute environment variables in the SQL template
 envsubst < /tmp/init.sql.template > /tmp/init.sql
 
-# Start MariaDB with the generated init file
 exec mysqld --user=mysql --init-file=/tmp/init.sql
