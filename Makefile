9ca460a5a94ec2855cdc85b25706b60cc0923db4
R0-main <justromixx@gmail.com>
2025-07-15 10:29:31 +0200

    cleanup a but

--- Makefile
+++ Makefile
@@ -5,8 +5,6 @@ SRCS=./srcs
 COMPOSE_YML=$(SRCS)/docker-compose.yml
 DK_COMPOSE=docker-compose -f $(COMPOSE_YML)
 
-# add hosts..
-
 all: setup up
 
 setup:
--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# Only substitute the DOMAIN_NAME variable, leave nginx variables alone
 envsubst '${DOMAIN_NAME}' < /etc/nginx/templates/nginx.conf > /etc/nginx/sites-enabled/default
 
-# Test nginx configuration
 nginx -t
 
-# Start nginx
 exec nginx -g 'daemon off;'
