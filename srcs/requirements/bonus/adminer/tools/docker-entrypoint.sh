a6f0e29ed4976cf23f7455826e43df754e5ad4d2
R0-main <justromixx@gmail.com>
2025-07-16 12:50:30 +0200

    adminer

--- srcs/requirements/bonus/adminer/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/adminer/tools/docker-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
-# Start Redis server
 echo "Starting Adminer server..."
 
-exec a2enconf adminer.conf
+echo "ServerName localhost" > /etc/apache2/conf-available/servername.conf
+a2enconf servername
+a2enconf adminer.conf
 
 service apache2 reload
 
+exec apache2ctl -D FOREGROUND
