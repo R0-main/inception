df6eec6e18b6fea48170d3b0007f1fc553bf8fa8
R0-main <justromixx@gmail.com>
2025-07-18 12:02:05 +0200

    working on n8n setup

--- srcs/requirements/bonus/n8n/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/n8n/tools/docker-entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-# Start Redis server
-echo "Starting Redis server..."
+n8n &
 
-exec redis-server --protected-mode no --bind 0.0.0.0 --port 6379 --daemonize no
+# PID du process lancé
+N8N_PID=$!
+
+# Attendre que la table "user" soit créée
+until mysql -h mariadb -u n8n_user -pn8n_password -e "USE n8n; SHOW TABLES LIKE 'user';" | grep user; do
+  echo "Waiting for table 'user' to be ready..."
+  sleep 5
+done
+
+# Une fois prêt, on injecte les données
+echo "Running SQL init script..."
+mysql -h mariadb -u n8n_user -pn8n_password n8n < /home/n8n/init.sql
+
+# Attendre que n8n (en arrière-plan) se termine (ça le garde en premier plan)
+wait $N8N_PID
