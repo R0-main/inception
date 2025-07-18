00eba577582ad266a6b64d0d1e234c0e14e336eb
R0-main <justromixx@gmail.com>
2025-07-18 12:02:36 +0200

    i'll see n8n later

--- srcs/requirements/bonus/n8n/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/n8n/tools/docker-entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
+# sleep
 
-n8n &
+# mysql -h mariadb -u n8n_user -pn8n_password n8n < /home/n8n/init.sql
 
-# PID du process lancé
-N8N_PID=$!
-
-# Attendre que la table "user" soit créée
-until mysql -h mariadb -u n8n_user -pn8n_password -e "USE n8n; SHOW TABLES LIKE 'user';" | grep user; do
-  echo "Waiting for table 'user' to be ready..."
-  sleep 5
-done
-
-# Une fois prêt, on injecte les données
-echo "Running SQL init script..."
-mysql -h mariadb -u n8n_user -pn8n_password n8n < /home/n8n/init.sql
-
-# Attendre que n8n (en arrière-plan) se termine (ça le garde en premier plan)
-wait $N8N_PID
+# # Start n8n
+exec n8n
