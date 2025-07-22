e590dfe3a4b7def62f264f5a3a02d322ec9c765d
R0-main <justromixx@gmail.com>
2025-07-22 11:35:06 +0200

    ftp server

--- srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-# sleep
-
-# mysql -h mariadb -u n8n_user -pn8n_password n8n < /home/n8n/init.sql
-
-# # Start n8n
-exec n8n
+echo "Starting vsftpd FTP server..."
+exec vsftpd /etc/vsftpd.conf
